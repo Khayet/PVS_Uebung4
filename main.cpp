1b1830e28831b83e24e5557f956ef99273b4670f
Khayet <jakob.herpel@uni-weimar.de>
2015-12-11 13:03:08 +0100

    Add execution example and first comments.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include<stdio.h>
-#include<mpi.h> // ???
+#include<mpi.h> // inkludiere den MPI-Header
 
 int main(int argc, char** argv){
 int nodeID, numNodes;
 
-/* ??? */
-MPI_Init(&argc, &argv); // ???
+/* Grundfunktionen von MPI: */
+MPI_Init(&argc, &argv);
+/* Initialisiert das MPI-Laufzeitsystem (Folie), uebergibt MPI die per Kommandozeile uebergebenen Argumente.
+Vorher duerfen keine MPI-Funktionen aufgerufen werden. */
 MPI_Comm_size(MPI_COMM_WORLD, &numNodes); // ???
 MPI_Comm_rank(MPI_COMM_WORLD, &nodeID); // ???
 
@@ -17,3 +19,13 @@ MPI_Finalize();
 
 return 0;
 }
+
+
+/*  Beispielausfuehrung (bash shell):
+ *  alias run='/opt/openmpi/bin/mpirun -np 4 main'
+ *  run
+ *  Hello world from process 2 of 4
+ *  Hello world from process 0 of 4
+ *  Hello world from process 1 of 4
+ *  Hello world from process 3 of 4
+*/
