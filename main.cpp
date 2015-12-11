7071cfc7169e8fbe6b2f387b464036395cbedd00
Khayet <jakob.herpel@uni-weimar.de>
2015-12-11 13:08:36 +0100

    Add additional comments.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int nodeID, numNodes;
 MPI_Init(&argc, &argv);
 /* Initialisiert das MPI-Laufzeitsystem (Folie), uebergibt MPI die per Kommandozeile uebergebenen Argumente.
 Vorher duerfen keine MPI-Funktionen aufgerufen werden. */
-MPI_Comm_size(MPI_COMM_WORLD, &numNodes); // ???
-MPI_Comm_rank(MPI_COMM_WORLD, &nodeID); // ???
+MPI_Comm_size(MPI_COMM_WORLD, &numNodes); // Gibt die Anzahl aller Prozesse im Kommunikator 'MPI_COMM_WORLD' zurueck.
+MPI_Comm_rank(MPI_COMM_WORLD, &nodeID); // Gibt die Nummer des ausfuehrenden Prozesses im Kommunikator 'MPI_COMM_WORLD' zurueck.
 
 /* ??? */
 printf("Hello world from process %d of %d\n", nodeID, numNodes);
